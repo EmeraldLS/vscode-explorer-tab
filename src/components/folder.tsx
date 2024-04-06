eb9be7f4624fa8d9d87217642f7704642be25ef8
Oluwasegun Lawrence <lawrencesegun025@gmail.com>
2024-04-06 16:14:14 +0100

    add file and folder

--- src/components/folder.tsx
+++ src/components/folder.tsx
@@ -1,12 +1,57 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { explorer } from "../data/folderData";
+import uuid from "react-uuid";
+
+enum CreateType {
+  Folder = "folder",
+  File = "file",
+}
 
 export const Folder = ({
-  explorerData: { name, id, children, isFolder },
+  explorerData: { name, children, isFolder },
 }: {
   explorerData: explorer;
 }) => {
   const [expand, setExpand] = useState(false);
+  const [showInput, setShowInput] = useState(false);
+
+  const [type, setType] = useState<CreateType>(CreateType.Folder);
+  const [input, setInput] = useState("");
+
+  const addFolder = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    setType(CreateType.Folder);
+    setShowInput(true);
+    setExpand(true);
+  };
+
+  const addFile = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    setType(CreateType.File);
+    setShowInput(true);
+    setExpand(true);
+  };
+
+  const create = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (type == CreateType.Folder) {
+      children?.unshift({
+        id: uuid(),
+        name: input,
+        isFolder: true,
+      });
+    } else if (type == CreateType.File) {
+      children?.unshift({
+        id: uuid(),
+        name: input,
+        isFolder: false,
+      });
+    }
+
+    setInput("");
+    setShowInput(false);
+  };
 
   if (isFolder)
     return (
@@ -15,11 +60,38 @@ export const Folder = ({
           className="folder"
           onClick={() => {
             setExpand(!expand);
+            setShowInput(false);
           }}
         >
-          <span>🗂️ {name}</span>
+          <span>
+            🗂️ {name}
+            <span className="buttons">
+              <button onClick={addFolder}>Folder +</button>
+              <button onClick={addFile} style={{ backgroundColor: "#fde908" }}>
+                File +
+              </button>
+            </span>
+          </span>
         </div>
-        <div className={!expand ? "hidden" : ""} style={{ paddingLeft: 25 }}>
+
+        <div
+          className={`${!expand ? "hidden" : ""} child`}
+          style={{ paddingLeft: 25 }}
+        >
+          {showInput && (
+            <form onSubmit={create}>
+              <input
+                type="text"
+                className=""
+                id="name"
+                onChange={(e) => setInput(e.target.value)}
+                value={input}
+                autoComplete="true"
+                placeholder={`Create ${type}`}
+                onBlur={() => setShowInput(false)}
+              />
+            </form>
+          )}
           {children?.map((child, _) => (
             <Folder explorerData={child} key={child.id} />
           ))}
